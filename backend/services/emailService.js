900c04d2bb3b506fcdd13e4104cbb7311007edb7
Shivani Singh <shivani.singh@w3villa.com>
2026-04-12 18:52:25 +0530

    port change to ipv 6 to ipv4

--- backend/services/emailService.js
+++ backend/services/emailService.js
@@ -69,6 +69,7 @@ const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 465,
   secure: true, // Use SSL/TLS
+  family: 4,
   auth: {
     user: process.env.EMAIL_USER, 
     pass: process.env.EMAIL_PASS, // 16-digit App Password
